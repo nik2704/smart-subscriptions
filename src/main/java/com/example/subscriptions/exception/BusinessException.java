c7259400f463f6cd0fb90328bf42e2d4d5e41221
Nikolay Zenchenko <nik2704@yandex.ru>
2026-07-04 19:53:02 +0300

    Notes

--- src/main/java/com/example/subscriptions/exception/BusinessException.java
+++ src/main/java/com/example/subscriptions/exception/BusinessException.java
@@ -1,7 +1,9 @@
 package com.example.subscriptions.exception;
 
+import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
+@Getter
 public class BusinessException extends RuntimeException {
 
     private final HttpStatus status;
@@ -11,7 +13,4 @@ public class BusinessException extends RuntimeException {
         this.status = status;
     }
 
-    public HttpStatus getStatus() {
-        return status;
-    }
 }
